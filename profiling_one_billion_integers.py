f8c0480f8111a7012333a551346f0a2ce6f45722
Dartz <samuel.viveiros@gmail.com>
2024-08-21 17:48:14 -0300

    Update profiling_one_billion_integers.py

--- profiling_one_billion_integers.py
+++ profiling_one_billion_integers.py
@@ -1,61 +1,47 @@
 import time
 from random import randint
+from typing import Optional, TextIO, Tuple
 
-import psutil
+import psutil 
 
 
-start_cpu = psutil.cpu_percent(interval=None)
-start_mem = psutil.virtual_memory().used
+RANDOM_INTEGER = randint(1, 1_000_000_000)
+print(f'Random integer generated: {RANDOM_INTEGER:,}'.replace(',', '.'))
 
-search = int(input('Para iniciarmos a busca, informe um número inteiro entre 1 e 1000000000 (um bilhão): '))
 
-start_time = time.time()
+def search_integer(target: int, file: TextIO) -> Tuple[bool, Optional[int]]:
+    for line, integer in enumerate(file, start=1):
+        if target == int(integer):
+            return True, line
+    return False, None
 
-total_of_files = 4
-memory = []  # Inicializa memória
+
 found = False
-for i in range(1, total_of_files + 1):
-    print(f'Gerando arquivo {i} de {total_of_files} com números inteiros aleatórios...')
-    # Gerando arquivo de 250 milhões de linhas, um pouco mais de
-    # 2.6 GB de tamanho em disco (cabe na RAM com certo conforto)
-    with open('250_millions_of_random_integers.txt', 'w') as integers:
-        for j in range(1, 250_000_000 + 1):
-            integers.write(f'{randint(1, 1_000_000_000)}\n')
+line = None
 
-    print(f'Carregando arquivo {i} de {total_of_files} na memória...')
-    with open('250_millions_of_random_integers.txt', 'r') as integers:
-        for integer in integers:
-            memory.append(int(integer.strip()))
+print('Searching integer in file `one_billion_integers.txt`...')
+with open('one_billion_integers.txt', 'r') as integers:
+    start_cpu = psutil.cpu_percent(interval=None)
+    start_mem = psutil.virtual_memory().used
+    start_time = time.time()
 
-    print('Buscando número inteiro na memória...')
-    for integer in memory:
-        if search == integer:
-            print(f'>>> Número {i} encontrado! <<<')
-            found = True
-            break
+    found, line = search_integer(target=RANDOM_INTEGER, file=integers)
 
-    if found:
-        memory = []  # Libera memória utilizada
-        break
+    end_time = time.time()
+    end_cpu = psutil.cpu_percent(interval=None)
+    end_mem = psutil.virtual_memory().used
 
-    memory = []  # Libera memória utilizada
-
-end_time = time.time()
-
-end_cpu = psutil.cpu_percent(interval=None)
-end_mem = psutil.virtual_memory().used
+if found:
+    print(f'Integer FOUND on line {line} !!!')
+else:
+    print('Integer NOT FOUND !!!')
 
 cpu_usage = end_cpu - start_cpu
 mem_usage = (end_mem - start_mem) / (1024 * 1024)  # Convert to MB
 execution_time = end_time - start_time
 
-print(f'CPU: {cpu_usage}%')
-print(f'RAM: {mem_usage:.2f} MB')
-print(f'Tempo de execução: {execution_time:.2f} segundos')
+print('-' * 42)
 
-
-
-
-
-# 1_000_000 ocupa 8 MB em disco
-        
+print(f'CPU usage: {cpu_usage}%')
+print(f'RAM usage: {mem_usage:.2f} MB')
+print(f'Execution time: {execution_time:.2f} seconds')
